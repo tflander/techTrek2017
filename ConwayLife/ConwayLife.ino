c9021a06be26ee791bc2c468c5bf7a8c4ac1ac32
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-05-01 22:26:02 -0400

    updated conway

--- ConwayLife/ConwayLife.ino
+++ ConwayLife/ConwayLife.ino
@@ -77,12 +77,65 @@ int getBlueFromLong(unsigned long value) {
   return value & 255;
 }
 
+int isAlive(unsigned long value) {
+  return (value >> 24) & 255;
+}
+
 unsigned long toLong(int isAlive, int red, int green, int blue) {
   return (unsigned long)blue | ((unsigned long)green << 8) | ((unsigned long)red << 16) | ((unsigned long)isAlive << 24);
 }
 
+  /*
+xAny live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+Any live cell with two or three live neighbours lives on to the next generation.
+xAny live cell with more than three live neighbours dies, as if by overpopulation.
+xAny dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+   */
+void mutateGrid() {
 
-void loop() {
+  int newCells[pixelsPerStrand][numStrands];
+  for( int r = 0; r < numStrands; ++r) {
+    for (int c = 0; c < pixelsPerStrand; ++c) {
+      int neighbors = countNeighbors(r, c);
+        int isLive = isAlive(cells[c][r]);
+        int newVal = 0;  // assume fewer than two neighbors or more than three neighbors
+        if(neighbors == 3) {
+          newVal = 1;
+        } else if (neighbors == 2) {
+          newVal = isLive;
+        }
+        newCells[c][r] = newVal;
+      }
+    }
+
+    // TODO: update cells that transitioned
+    
+}
+
+
+int countNeighbors(int col, int row) {
+  int neighbors = 0;
+        
+   for (int r = row - 1; r <= row + 1; ++r) {
+     for(int c = col - 1; c <= col + 1; ++c) {
+       if(r != row || c != col) {
+         int rpos = r;
+         int cpos = c;
+         if(rpos < 0) rpos = numStrands - 1;
+         if(rpos >= pixelsPerStrand) rpos = 0;
+         if(cpos < 0) cpos =  pixelsPerStrand - 1;
+         if(cpos >= numStrands) cpos = 0;
+         if(isAlive(cells[cpos][rpos])) {
+             neighbors = neighbors + 1;
+         }
+       }
+      }
+    }
+    return neighbors;
+  }
+
+
+void showCells() {
   for (int row = 0; row < numStrands; ++row) {
     for(int pixel = 0; pixel < pixelsPerStrand; ++pixel) {
         long cellAsLong = cells[pixel][row];
@@ -91,5 +144,10 @@ void loop() {
     matrix[row]->show();
   }
   
-  delay(10000);
+  delay(1000);  
+}
+
+void loop() {
+  showCells();
+  mutateGrid();
 }
