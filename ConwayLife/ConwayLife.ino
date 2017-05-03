211102d64468f089a6c03cfd22844a76b9c18b6b
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-05-02 21:57:31 -0400

    First conway success

--- ConwayLife/ConwayLife.ino
+++ ConwayLife/ConwayLife.ino
@@ -3,9 +3,11 @@
   #include <avr/power.h>
 #endif
 
+//#define DEBUG 0
+
 const int numStrands = 8;
 const int pixelsPerStrand = 24;
-const int onBrightness = 16;
+const int onBrightness = 8;
 const int pinForRowZero = 2;
 const int LIVE = 1;
 const int DEAD = -1;
@@ -14,6 +16,7 @@ const int RED = LIVE;
 const int GREEN = NEUTRAL;
 const int BLUE = DEAD;
 const int isRGBW = 0;
+const int delayBetweenCycles = 1000;
 
 unsigned long cells[pixelsPerStrand][numStrands];
 
@@ -21,19 +24,32 @@ Adafruit_NeoPixel *matrix[numStrands];
 
 void setup() {
 
+#ifdef DEBUG
+Serial.begin(9600);
+#endif
+
+  #ifdef DEBUG
+    Serial.println("+setup()");
+  #endif
+
  neoPixelType pixelType = NEO_GRB + NEO_KHZ800;
   if(isRGBW) {
     pixelType = NEO_GRBW + NEO_KHZ800;
   }
-  for (int strand=0; strand < numStrands; ++strand) {
-    Adafruit_NeoPixel* pixels = new Adafruit_NeoPixel(pixelsPerStrand, strand + pinForRowZero, NEO_GRB + NEO_KHZ800);
+  for (int y=0; y < numStrands; ++y) {
+    Adafruit_NeoPixel* pixels = new Adafruit_NeoPixel(pixelsPerStrand, y + pinForRowZero, NEO_GRB + NEO_KHZ800);
     pixels->begin();
-    matrix[strand] = pixels;
+    matrix[y] = pixels;
 
-    for(int pixel = 0; pixel < pixelsPerStrand; ++pixel) {
-      setCell(random(2), strand, pixel);
+    for(int x = 0; x < pixelsPerStrand; ++x) {
+      setCell(random(2), x, y);
     }
   }
+
+  #ifdef DEBUG
+    Serial.println("-setup()");
+  #endif
+  
 }
 
 int getRandomValueForColor(int isLive, int color) {
@@ -46,23 +62,22 @@ int getRandomValueForColor(int isLive, int color) {
     if(isLive) return onBrightness;
     return 0;
   }
-  return random(onBrightness);
+  return random(onBrightness/2);
 }
 
-// TODO: will need these later.
-//void setDead(int row, int col) {
-//  setCell(0, row, col);
-//}
-//
-//void setAlive(int row, int col) {
-//  setCell(1, row, col);
-//}
+void setDead(int x, int y) {
+  setCell(0, x, y);
+}
 
-void setCell(int isLive, int row, int col) {
+void setAlive(int x, int y) {
+  setCell(1, x, y);
+}
+
+void setCell(int isLive, int x, int y) {
   int red = getRandomValueForColor(isLive, RED);
   int green = getRandomValueForColor(isLive, GREEN);
   int blue = getRandomValueForColor(isLive, BLUE);
-  cells[col][row] = toLong(isLive, red, green, blue);
+  cells[x][y] = toLong(isLive, red, green, blue);
 }
 
 int getRedFromLong(unsigned long value) {
@@ -93,39 +108,70 @@ xAny dead cell with exactly three live neighbours becomes a live cell, as if by 
    */
 void mutateGrid() {
 
+  // determine cells that transitioned
   int newCells[pixelsPerStrand][numStrands];
-  for( int r = 0; r < numStrands; ++r) {
-    for (int c = 0; c < pixelsPerStrand; ++c) {
-      int neighbors = countNeighbors(r, c);
-        int isLive = isAlive(cells[c][r]);
+  for( int y = 0; y < numStrands; ++y) {
+    for (int x = 0; x < pixelsPerStrand; ++x) {
+      int neighbors = countNeighbors(x, y);
+        int isLive = isAlive(cells[x][y]);
         int newVal = 0;  // assume fewer than two neighbors or more than three neighbors
         if(neighbors == 3) {
           newVal = 1;
         } else if (neighbors == 2) {
           newVal = isLive;
         }
-        newCells[c][r] = newVal;
+
+        int shouldFlip = 0;
+        if(newVal != isLive) {
+          shouldFlip = 1;
+        }
+        newCells[x][y] = shouldFlip;
       }
     }
 
-    // TODO: update cells that transitioned
+  #ifdef DEBUG
+    Serial.println("Cells to Flip");
+  for( int y = 0; y < numStrands; ++y) {
+    for (int x = 0; x < pixelsPerStrand; ++x) {
+      Serial.print(newCells[x][y]);
+    }
+    Serial.println("");
+  }    
+  Serial.println("==========");
+  #endif
     
+
+  // update cells that transitioned
+  for( int y = 0; y < numStrands; ++y) {
+    for (int x = 0; x < pixelsPerStrand; ++x) {
+      if(newCells[x][y]) {
+        int isLive = isAlive(cells[x][y]);
+        if(isLive) {
+          setDead(x, y);
+        } else {
+          setAlive(x, y);
+        }
+        
+      }
+    }
+  }
+      
 }
 
 
-int countNeighbors(int col, int row) {
+int countNeighbors(int x, int y) {
   int neighbors = 0;
         
-   for (int r = row - 1; r <= row + 1; ++r) {
-     for(int c = col - 1; c <= col + 1; ++c) {
-       if(r != row || c != col) {
-         int rpos = r;
-         int cpos = c;
-         if(rpos < 0) rpos = numStrands - 1;
-         if(rpos >= pixelsPerStrand) rpos = 0;
-         if(cpos < 0) cpos =  pixelsPerStrand - 1;
-         if(cpos >= numStrands) cpos = 0;
-         if(isAlive(cells[cpos][rpos])) {
+   for (int x1 = x - 1; x1 <= x + 1; ++x1) {
+     for(int y1 = y - 1; y1 <= y + 1; ++y1) {
+       if(x1 != x || y1 != y) {
+         int xpos = x1;
+         int ypos = y1;
+         if(ypos < 0) ypos = numStrands - 1;
+         if(xpos >= pixelsPerStrand) xpos = 0;
+         if(xpos < 0) xpos =  pixelsPerStrand - 1;
+         if(ypos >= numStrands) ypos = 0;
+         if(isAlive(cells[xpos][ypos])) {
              neighbors = neighbors + 1;
          }
        }
@@ -136,15 +182,34 @@ int countNeighbors(int col, int row) {
 
 
 void showCells() {
-  for (int row = 0; row < numStrands; ++row) {
-    for(int pixel = 0; pixel < pixelsPerStrand; ++pixel) {
-        long cellAsLong = cells[pixel][row];
-        matrix[row]->setPixelColor(pixel, getRedFromLong(cellAsLong), getGreenFromLong(cellAsLong), getBlueFromLong(cellAsLong));
+
+  #ifdef DEBUG
+    Serial.println("showCells");
+  for( int y = 0; y < numStrands; ++y) {
+    for (int x = 0; x < pixelsPerStrand; ++x) {
+        long cellAsLong = cells[x][y];
+        Serial.print("r=");
+        Serial.print(getRedFromLong(cellAsLong));
+        Serial.print(";g=");
+        Serial.print(getGreenFromLong(cellAsLong));
+        Serial.print(";b=");
+        Serial.print(getBlueFromLong(cellAsLong));
+        Serial.print("|");
     }
-    matrix[row]->show();
+    Serial.println("");
+  }    
+  Serial.println("==========");
+  #endif
+  
+  for (int y = 0; y < numStrands; ++y) {
+    for(int x = 0; x < pixelsPerStrand; ++x) {
+        long cellAsLong = cells[x][y];
+        matrix[y]->setPixelColor(x, getRedFromLong(cellAsLong), getGreenFromLong(cellAsLong), getBlueFromLong(cellAsLong));
+    }
+    matrix[y]->show();
   }
   
-  delay(1000);  
+  delay(delayBetweenCycles);  
 }
 
 void loop() {
