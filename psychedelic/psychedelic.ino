e3bfc3b2c4eea4c616f5e6a1947f046c30db39c3
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-05-14 19:54:00 -0400

    first pass at fading to the next color

--- psychedelic/psychedelic.ino
+++ psychedelic/psychedelic.ino
@@ -9,6 +9,7 @@ const int onBrightness = 16;
 const int pinForRowZero = 2;
 const int isRGBW = 0;
 const int cycleDelay = 400;
+const int fadeDelay = 40;
 
 Adafruit_NeoPixel *matrix[numStrands];
 
@@ -18,12 +19,19 @@ unsigned long toLong(int data, int red, int green, int blue) {
   return (unsigned long)blue | ((unsigned long)green << 8) | ((unsigned long)red << 16) | ((unsigned long)data << 24);
 }
 
+const int TO_RED = 1;
+const int TO_GREEN = 2;
+const int TO_BLUE = 4;
+const int TO_CYAN = TO_GREEN & TO_BLUE;
+const int TO_MAGENTA = TO_RED & TO_BLUE;
+const int TO_YELLOW = TO_GREEN & TO_GREEN;
+
 unsigned long red =     toLong(0, onBrightness, 0, 0);
-unsigned long green =   toLong(0, 0, onBrightness, 0);
-unsigned long blue =    toLong(0, 0, 0, onBrightness);
+unsigned long green =   toLong(TO_BLUE, 0, onBrightness, 0);
+unsigned long blue =    toLong(TO_YELLOW, 0, 0, onBrightness);
 unsigned long cyan =    toLong(0, 0, onBrightness, onBrightness);
-unsigned long magenta = toLong(0, onBrightness, 0, onBrightness);
-unsigned long yellow =  toLong(0, onBrightness, onBrightness , 0);
+unsigned long magenta = toLong(TO_GREEN, onBrightness, 0, onBrightness);
+unsigned long yellow =  toLong(TO_MAGENTA, onBrightness, onBrightness , 0);
 
 unsigned long colors[] = {magenta, green, blue, yellow};
 
@@ -53,8 +61,48 @@ int getBlueFromLong(unsigned long value) {
   return value & 255;
 }
 
-int getData(unsigned long value) {
+int getDataFromLong(unsigned long value) {
   return (value >> 24) & 255;
+}
+
+void fadeToNextColor() {
+  for(int i = 0; i < onBrightness; ++i) {
+    stepFade();
+  }
+}
+
+int adjustColor(int currentColor, const int toColor) {
+  
+        if(toColor) {
+          if(currentColor < onBrightness) return currentColor + 1;
+        } else {
+          if (currentColor > 0) return currentColor - 1;
+        }
+
+        return currentColor;
+}
+
+void stepFade() {
+
+  for (int y = 0; y < numStrands; ++y) {
+    for(int x = 0; x < pixelsPerStrand; ++x) {
+        long cellAsLong = cells[x][y];
+        int red = getRedFromLong(cellAsLong);
+        int green = getGreenFromLong(cellAsLong);
+        int blue = getBlueFromLong(cellAsLong);
+        int data = getDataFromLong(cellAsLong);
+
+        red = adjustColor(red, data & TO_RED);
+        green = adjustColor(green, data & TO_GREEN);
+        blue = adjustColor(blue, data & TO_BLUE);
+
+        cells[x][y] = toLong(data, red, green, blue);
+        matrix[y]->setPixelColor(x, red, green, blue);
+    }
+    matrix[y]->show();
+  }
+  
+  delay(fadeDelay);  
 }
 
 void showCells() {
@@ -67,7 +115,8 @@ void showCells() {
     matrix[y]->show();
   }
   
-  delay(cycleDelay);  
+  delay(cycleDelay);  // TODO: probably remove this?
+  fadeToNextColor();
 }
 
 void setHline(unsigned long color, int x1, int x2, int y) {
@@ -114,11 +163,6 @@ void loop() {
   for (int i=3; i>=0; --i) {
     paintRings(i);
   }
-
-//  paintRings(red, green, blue, yellow);
-//  paintRings(green, blue, yellow, red);
-//  paintRings(blue, yellow, red, green);
-//  paintRings(yellow, red, green, blue);
 
   
 }
