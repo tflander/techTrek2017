da93199d605081cc91e93ed4a2f1d4065029c0da
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-05-14 18:35:47 -0400

    make psy code more generic

--- psychedelic/psychedelic.ino
+++ psychedelic/psychedelic.ino
@@ -14,6 +14,18 @@ Adafruit_NeoPixel *matrix[numStrands];
 
 unsigned long cells[pixelsPerStrand][numStrands];
 
+unsigned long toLong(int data, int red, int green, int blue) {
+  return (unsigned long)blue | ((unsigned long)green << 8) | ((unsigned long)red << 16) | ((unsigned long)data << 24);
+}
+
+unsigned long red =     toLong(0, onBrightness, 0, 0);
+unsigned long green =   toLong(0, 0, onBrightness, 0);
+unsigned long blue =    toLong(0, 0, 0, onBrightness);
+unsigned long cyan =    toLong(0, 0, onBrightness, onBrightness);
+unsigned long magenta = toLong(0, onBrightness, 0, onBrightness);
+unsigned long yellow =  toLong(0, onBrightness, onBrightness , 0);
+
+unsigned long colors[] = {magenta, green, blue, yellow};
 
 void setup() {
 
@@ -43,10 +55,6 @@ int getBlueFromLong(unsigned long value) {
 
 int getData(unsigned long value) {
   return (value >> 24) & 255;
-}
-
-unsigned long toLong(int data, int red, int green, int blue) {
-  return (unsigned long)blue | ((unsigned long)green << 8) | ((unsigned long)red << 16) | ((unsigned long)data << 24);
 }
 
 void showCells() {
@@ -81,34 +89,36 @@ void paintRing(unsigned long color, int x1, int y1, int x2, int y2) {
   setVline(color, y1, y2, x2);
 }
 
-void paintRings(unsigned long innerRingColor, unsigned long ring2Color, unsigned long ring3Color, unsigned long outerRingColor) {
+unsigned long getColorWithOffset(int offset) {
+  if(offset > 3) offset -= 4;
+  return colors[offset];
+}
+
+void paintRings(int offset) {
 
   int centerX1 = 3;
   int centerX2 = 20;
   int centerY1 = 3;
   int centerY2 = 4;
 
-  paintRing(innerRingColor, centerX1, centerY1, centerX2, centerY2);
-  paintRing(ring2Color, centerX1-1, centerY1-1, centerX2+1, centerY2+1);
-  paintRing(ring3Color, centerX1-2, centerY1-2, centerX2+2, centerY2+2);
-  paintRing(outerRingColor, centerX1-3, centerY1-3, centerX2+3, centerY2+3);  
+  paintRing(getColorWithOffset(offset), centerX1, centerY1, centerX2, centerY2);
+  paintRing(getColorWithOffset(offset+1), centerX1-1, centerY1-1, centerX2+1, centerY2+1);
+  paintRing(getColorWithOffset(offset+2), centerX1-2, centerY1-2, centerX2+2, centerY2+2);
+  paintRing(getColorWithOffset(offset+3), centerX1-3, centerY1-3, centerX2+3, centerY2+3);  
   showCells();
   delay(cycleDelay);
 }
 
 void loop() {
 
-  unsigned long red =     toLong(0, onBrightness, 0, 0);
-  unsigned long green =   toLong(0, 0, onBrightness, 0);
-  unsigned long blue =    toLong(0, 0, 0, onBrightness);
-  unsigned long cyan =    toLong(0, 0, onBrightness, onBrightness);
-  unsigned long magenta = toLong(0, onBrightness, 0, onBrightness);
-  unsigned long yellow =  toLong(0, onBrightness, onBrightness , 0);
+  for (int i=3; i>=0; --i) {
+    paintRings(i);
+  }
 
-  paintRings(red, green, blue, yellow);
-  paintRings(green, blue, yellow, red);
-  paintRings(blue, yellow, red, green);
-  paintRings(yellow, red, green, blue);
+//  paintRings(red, green, blue, yellow);
+//  paintRings(green, blue, yellow, red);
+//  paintRings(blue, yellow, red, green);
+//  paintRings(yellow, red, green, blue);
 
   
 }
